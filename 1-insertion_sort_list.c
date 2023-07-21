ee032692a3e5e3b3ec6c8960c9140f0fa518eb5b
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-07-21 20:35:40 +0000

    sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -37,7 +37,7 @@ void insertion_sort_list(listint_t **list)
 		insert = iter->prev;
 		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(list, &insert, iter);
+			swap(list, &insert, iter);
 			print_list((const listint_t *)*list);
 		}
 	}
